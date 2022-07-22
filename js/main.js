d0bd973b010b02682b92b46a213609631e910738
mzhdipu <mojammelhossaindipu@gmail.com>
2022-07-22 20:18:32 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var owl1 = $("#owl1");
+owl1.owlCarousel({
+    items:5,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:false
+});
+
+
+
+/**** owl2******/
+
+var owl2 = $("#owl2");
+owl2.owlCarousel({
+    items:3,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:false
+});
